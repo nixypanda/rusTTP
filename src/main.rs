2c452960f8abcf953f733b66206c1617da57d0c5
Sherub Thakur <sherub.thakur@gmail.com>
2023-09-28 05:55:42 +0530

    Bind to a port

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+fn handle_client(mut stream: TcpStream) -> anyhow::Result<()> {
+    Ok(())
+}
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+        handle_client(stream)?;
+    }
+
+    Ok(())
 }
